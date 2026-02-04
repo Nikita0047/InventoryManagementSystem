569ce940eadac2fe7782d10e4a8df63dd4287610
Nikita Kumari <nikitasuprstr@gmail.com>
2026-02-04 11:49:23 +0530

    Created Models/Entities

---
+++ InventoryModels/DTOs/SignUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryModels.DTOs
+{
+    public class SignUp
+    {
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name must be between 2-100 characters")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
