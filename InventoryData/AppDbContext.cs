01058b368cd55578fd37112d0348f42c2b954582
Nikita Kumari <nikitasuprstr@gmail.com>
2026-02-06 18:51:24 +0530

    Establish Databse connection

---
+++ InventoryData/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using InventoryModels.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryData
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+       public DbSet<Users> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfig());
+            // Additional configuration can go here
+        }
+    }
+}
