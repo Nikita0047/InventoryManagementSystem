62cb37529a77b852738f8cfc6b074cd7bc622253
Nikita Kumari <nikitasuprstr@gmail.com>
2026-02-18 19:15:37 +0530

    made SignIn and SignUp serviceLogic

---
+++ InventoryService/Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using InventoryData;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryService.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        internal DbSet<T> dbSet;
+        public Repository(AppDbContext context)
+        {
+            _context= context;
+            this.dbSet = context.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+           
+        }
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+           IQueryable<T> query = dbSet;
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            IQueryable<T> query = dbSet;
+
+            // Assuming T has a property named "Id" of type int.
+            // If not, you need to provide more info about how to get by id.
+            var entity = await query.FirstOrDefaultAsync(e => EF.Property<int>(e, "Id") == id);
+            return entity!;
+        }
+
+        public void Update(T entity)
+        {
+            dbSet.Update(entity);
+        }
+    }
+}
---
+++ InventoryService/Repository/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using InventoryData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryService.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;  
+        }
+
+        public async Task SaveAsync()
+        {
+           await _context.SaveChangesAsync();
+        }
+    }
+}
